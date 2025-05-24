1b62c234c155ba0c25b764c1492023a7d5e83443
Adejare Faruk <nou183023868@noun.edu.ng>
2025-05-24 07:40:27 +0100

    first commit

---
+++ 0x04-TypeScript/task_0/main.ts
@@ -0,0 +1,20 @@
+interface student{
+    firstname: string;
+    lastname: string;
+    age: number;
+    location: string;
+}
+
+const student1: student = {
+    firstname: "Adejare",
+    lastname: "Faruk",
+    age: 26,
+    location: "Arepo Ogun State"
+}
+
+const student2: student = {
+    firstname: "Aderibigbe",
+    lastname: "Toluwalope",
+    age: 27,
+    location: "Agbowo Ibadan"
+}
